c59b21282a3d9b5963b34a2f2bb70b152bf8206a
Thomas Masson <charliewchocolate@gmail.com>
2023-01-16 14:29:52 +0100

    Les produits selec s'affichent page panier *sans innerHTML

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,110 +1,144 @@
-function saveBasket(basket) {
-    localStorage.setItem('basket', JSON.stringify(basket)); // clé - valeur
-}
+const itemsList = JSON.parse(localStorage.getItem('basket'));
+//console.log(itemsList);
 
-function getBasket() {
-    let basket = localStorage.getItem('basket');
-    if (basket == null) {        // le panier n'existe pas encore
-        return [];              // création d'un tableau vide à savoir un panier
-    } else {
-        return JSON.parse(basket);
-    }
-}
-//console.log(getBasket());
-
-//récupérer le panier du localStorage
-const list = getBasket();
-//console.log(list);
-
-// const productsList = JSON.parse(localStorage.getItem('basket'));
-// console.log(productsList);
-
-list.forEach((product) => {
-    //console.log(product);
-    
-    const id = product.id;
-    const color = product.color;
-    const quantity = product.quantity;
+//boucler sur chaque item présent dans le tableau récupéré du LocalStorage
+itemsList.forEach((item) => {
+    let id = item.id;
+    let color = item.color;
+    let quantity = item.quantity;
     //console.log(id, color, quantity);
+    displayItem(id, color, quantity);
 
-    async function displayProduct() {
+    async function displayItem(id, color, quantity) {
         try {
             const reponseJSON = await fetch(`http://localhost:3000/api/products/${id}`);
-            const reponseJS = await reponseJSON.json();
-            //console.log(reponseJS);
-            const productArticle = 
-            `
-                <article class="cart__item" data-id="${id}" data-color="${color}">
-                    <div class="cart__item__img">
-                        <img src="${reponseJS.imageUrl}" alt="${reponseJS.altTxt}">
-                    </div>
-                    <div class="cart__item__content">
-                        <div class="cart__item__content__description">
-                            <h2>${reponseJS.name}</h2>
-                            <p>${color}</p>
-                            <p>${reponseJS.price} €</p>
-                        </div>
-                        <div class="cart__item__content__settings">
-                            <div class="cart__item__content__settings__quantity">
-                                <p>Qté : </p>
-                                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${quantity}">
-                            </div>
-                            <div class="cart__item__content__settings__delete">
-                                <p class="deleteItem">Supprimer</p>
-                            </div>
-                        </div>
-                    </div>
-                </article>
-            `;
-            const productContainer = document.querySelector('#cart__items');
-            productContainer.innerHTML += productArticle;
+            const value = await reponseJSON.json();
+            //console.log(value);
+    
+            let itemId = id;
+            let itemColor = color;
+            let itemQuantity = quantity;
+    
+            itemsData(value, itemId, itemColor, itemQuantity);
 
+            //récupérer pour chaque objet du localStorage (panier), ces infos de l'API
+            function itemsData(value, itemId, itemColor, itemQuantity) {
 
+                //création des éléments du DOM pour afficher un item
+                const article = createArticle(itemId, itemColor);
+                
+                const imageContainer = createImageContainer(value);
+                article.appendChild(imageContainer);
+                
+                const cartItemContent = createCartItemContent(value, itemColor, itemQuantity);
+                article.appendChild(cartItemContent);
+            }
+            
+            function createArticle(itemId, itemColor) {
+                const articleContainer = document.querySelector('#cart__items');
+
+                const article = document.createElement('article');
+                article.classList.add('cart__item');
+                article.dataset.id = itemId;
+                article.dataset.color = itemColor;
+
+                articleContainer.appendChild(article);
+
+                return article;
+            }
+            
+            function createImageContainer(value) {
+                const div = document.createElement('div');
+                div.classList.add('cart__item__img');
+                const image = document.createElement('img');
+                image.src = value.imageUrl;
+                image.alt = value.altTxt;
+                div.appendChild(image);
+                return div;
+            }
+
+            function createCartItemContent(value, itemColor, itemQuantity) {
+                const contentContainer = document.createElement('div');
+                contentContainer.classList.add('cart__item__content');
+
+                const description = createCartItemContentDescription(value, itemColor);
+                const settings = createCartItemContentSettings(itemQuantity);
+
+                contentContainer.appendChild(description);
+                contentContainer.appendChild(settings);
+
+                return contentContainer;
+            }
+
+            function createCartItemContentDescription(value, itemColor) {
+                const description = document.createElement('div');
+                description.classList.add('cart__item__content__description');
+
+                const title = document.createElement('h2');
+                title.textContent = value.name;
+
+                const colorParagraph = document.createElement('p');
+                colorParagraph.textContent = itemColor;
+
+                const priceParagraph = document.createElement('p');
+                priceParagraph.textContent = value.price + ' €';
+
+                description.appendChild(title);
+                description.appendChild(colorParagraph);
+                description.appendChild(priceParagraph);
+                
+                return description;
+            }
+
+            function createCartItemContentSettings(itemQuantity) {
+                const settings = document.createElement('div');
+                settings.classList.add('cart__item__content__settings');
+
+                createCartItemContentSettingsQuantity(settings, itemQuantity);
+                createCartItemContentSettingsDelete(settings)
+                return settings;
+            }
+
+            function createCartItemContentSettingsQuantity(settings, itemQuantity) {
+                const settingsQuantity = document.createElement('div');
+                settingsQuantity.classList.add('cart__item__content__settings__quantity');
+
+                const quantityParagraph = document.createElement('p');
+                quantityParagraph.textContent = 'Qté : ';
+
+                const quantityInput = document.createElement('input');
+                quantityInput.type = 'number';
+                quantityInput.classList.add = 'itemQuantity';
+                quantityInput.name = 'itemQuantity';
+                quantityInput.min = '1';
+                quantityInput.max = '100';
+                quantityInput.value = itemQuantity;
+
+                settingsQuantity.appendChild(quantityParagraph);
+                settingsQuantity.appendChild(quantityInput);
+                settings.appendChild(settingsQuantity);
+            }
+
+            function createCartItemContentSettingsDelete(settings) {
+                const settingsDelete = document.createElement('div');
+                settingsDelete.classList.add('cart__item__content__settings__delete');
+
+                const deleteParagraph = document.createElement('p');
+                deleteParagraph.classList.add('deleteItem');
+                deleteParagraph.textContent = 'Supprimer';
+
+                settingsDelete.appendChild(deleteParagraph);
+                settings.appendChild(settingsDelete);
+            }
         }
         catch(error) {
             console.log(error, 'erreur');
         }
-    }
-    displayProduct();
-    
-    /*fetch(`http://localhost:3000/api/products/${id}`)
-    .then(response => response.json())
-    .then(value => { 
-        //console.log(value)
-        //problème de tri des produits - la méthode .sort() ne fonctionne pas avant fetch
-        const productArticle = `
-                                <article class="cart__item" data-id="${id}" data-color="${color}">
-                                    <div class="cart__item__img">
-                                        <img src="${value.imageUrl}" alt="${value.altTxt}">
-                                    </div>
-                                    <div class="cart__item__content">
-                                        <div class="cart__item__content__description">
-                                            <h2>${value.name}</h2>
-                                            <p>${color}</p>
-                                            <p>${value.price} €</p>
-                                        </div>
-                                        <div class="cart__item__content__settings">
-                                            <div class="cart__item__content__settings__quantity">
-                                                <p>Qté : </p>
-                                                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${quantity}">
-                                            </div>
-                                            <div class="cart__item__content__settings__delete">
-                                                <p class="deleteItem">Supprimer</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </article>
-                                `;
-        
-    const productContainer = document.querySelector('#cart__items');
-    productContainer.innerHTML += productArticle;
+    };
+     
+});
 
-    const buttonDeleteProduct = document.querySelector('.deleteItem');
-    console.log(buttonDeleteProduct, "button");
 
-    
-    })*/
-})
 
 
 
